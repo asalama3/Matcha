58721cfacb2e8ef445a0203483dfb30310dd30ac
Andrea SALAMA <andreasalama2@gmail.com>
2016-10-26 20:48:58 +0200

    matcha front

---
+++ matcha-front/matcha_front_work/src/test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Test extends React.Component {
+  render() {
+    return (
+      <div>
+        <button type="button"> salut </button>
+      </div>
+    )
+  }
+}
+
+export default Test
