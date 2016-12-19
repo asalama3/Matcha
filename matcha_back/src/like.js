659a065602fe352d3d688431d293d78cbc3ae50b
Andrea SALAMA <andreasalama2@gmail.com>
2016-12-19 17:19:40 +0100

    like file

---
+++ matcha_back/src/like.js
@@ -0,0 +1,15 @@
+import MongoConnect from '../mongo_connect';
+var session = require('express-session');
+import mongodb from 'mongodb';
+
+
+const like = async(req, res) => {
+    // console.log("entered function like");
+    // res.send({status: true, details: "ok"});
+    // console.log(req.body);
+    // db.collection('users').findOne({ }, function (err, user){
+
+    })
+}
+
+export {like};
