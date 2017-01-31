861be46f798bc94ac29812a740fd65852a1eaaf4
JEREMY MARUANI <jeremymaruani@imac-de-jeremy.home>
2017-01-31 22:02:31 +0100

    started chat

--- matcha_front/src/chat.js
+++ matcha_front/src/chat.js
@@ -3,6 +3,19 @@ import axios from 'axios';
 import { browserHistory } from 'react-router';
 
 class Chat extends React.Component {
+  componentDidMount = async () => {
+
+  }
+   // list des mutuels likes - matcha
+   // check if person is online or offline
+   // if person is online, send message directly, if person is not online, save in database
+
+   // keep last 200 messages in chat
+   // input
+
+
+
+
   render() {
     return (
       <div> Coming Soon...</div>
