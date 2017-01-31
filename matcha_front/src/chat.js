7891f7d5895c53a20933f9aa236c2ce0e217b8ef
Andrea Salama <andrea@andreas-macbook-air.home>
2017-01-31 23:23:06 +0100

    chat tbc

--- matcha_front/src/chat.js
+++ matcha_front/src/chat.js
@@ -3,22 +3,44 @@ import axios from 'axios';
 import { browserHistory } from 'react-router';
 
 class Chat extends React.Component {
-  componentDidMount = async () => {
 
+  state = {
+    user: '',
+    pending: true,
   }
-   // list des mutuels likes - matcha
+
+  componentDidMount = async () => {
+    const checkAuth = await axios.get('http://localhost:8080/check_auth', {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`
+        },
+    });
+    if (checkAuth.data.status === true) {
+      const loggedUser = checkAuth.data.data;
+      this.setState({
+        user: loggedUser,
+        match: loggedUser.match,
+        pending: false,
+      });
+    }
+  }
    // check if person is online or offline
    // if person is online, send message directly, if person is not online, save in database
-
    // keep last 200 messages in chat
-   // input
-
-
-
 
   render() {
+    let matches = [];
+    if (!this.state.pending && this.state.match) {
+      matches = this.state.match.map((src, key) => <li key={key}> {src} </li>);
+    }
     return (
-      <div> Coming Soon...</div>
+      <div>
+        <div>
+          <ul>
+            {matches}
+          </ul>
+        </div>
+      </div>
     );
   }
 }
